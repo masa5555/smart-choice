b5f028c3fae09f727b9dadedcff9540455bfb649
masa5555 <masa57887@gmail.com>
2025-02-10 23:05:13 +0900

    add compare research

--- app/step/2-target/from-compare/_action.ts
+++ app/step/2-target/from-compare/_action.ts
@@ -1,7 +1,9 @@
 "use server";
 
 import { firestore } from "@/app/_config/firestore";
+import { researchFlow } from "@/app/_flow/researchFlow";
 import { redirect } from "next/navigation";
+import { after } from "next/server";
 
 export const handleSubmit = async ({
   items,
@@ -18,20 +20,42 @@ export const handleSubmit = async ({
     return redirect("/result/135353533");
   }
 
+  const plan = await firestore.collection("plans").add({
+    status: "created",
+    category,
+    items,
+  });
+
   const doc = await firestore.collection("results").add({
     status: "created",
     createdAt: new Date().toISOString(),
     plans: [
       {
-        id: "1",
+        id: plan.id,
         name: `${items.join(" vs ")}の比較`,
-        status: "created",
       },
     ],
   });
   if (!doc.id) {
     throw new Error("Failed to create a new result");
   }
+
+  after(async () => {
+    const res = await researchFlow({
+      category,
+      items: items.map((item) => ({
+        perspective: category,
+        userSelection: item,
+      })),
+    });
+    console.log({
+      compare_result: res,
+    });
+    await firestore.doc(`plans/${plan.id}`).set({
+      status: "finished",
+      result: res,
+    });
+  });
 
   return redirect(`/result/${doc.id}`);
 };
