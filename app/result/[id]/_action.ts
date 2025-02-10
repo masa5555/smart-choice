27c24356f824eb3f02ac1824cb46f2e4bf6382aa
masa5555 <masa57887@gmail.com>
2025-02-10 11:09:38 +0900

    fix

--- app/result/[id]/_action.ts
+++ app/result/[id]/_action.ts
@@ -1,12 +1,10 @@
 "use server";
 
 import { firestore } from "@/app/_config/firestore";
-const crypto = require("node:crypto");
+// const crypto = require("node:crypto");
 
 export default async function createNewResult() {
-  const uuid = crypto.randomUUID();
-  console.log({ uuid });
-  const doc = firestore.collection("results").add({
+  const doc = await firestore.collection("results").add({
     status: "created",
     createdAt: new Date().toISOString(),
   });
