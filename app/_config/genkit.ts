68a381bce8e562db7a713c486db9329bd6d3c678
masa5555 <masa57887@gmail.com>
2025-02-10 11:28:31 +0900

    add: genkit production

--- app/_config/genkit.ts
+++ app/_config/genkit.ts
@@ -1,5 +1,8 @@
+import { enableFirebaseTelemetry } from "@genkit-ai/firebase";
 import vertexAI, { gemini20Flash001 } from "@genkit-ai/vertexai";
 import { genkit } from "genkit";
+
+enableFirebaseTelemetry();
 
 export const vertexAiGemini20Flash = genkit({
   plugins: [vertexAI({ location: "us-central1" })],
